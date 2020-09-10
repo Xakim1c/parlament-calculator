a785123ce70b66385fa5e09ad969d812ca36a42a
xakim <xakim@pop-os.localdomain>
2020-09-10 14:09:06 +0600

    Updates

---
+++ precache-manifest.a76c8e88111b2e506329c020e6610db0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5699918dc7a8c6caa384269329bac2",
+    "url": "/parlament-calculator/index.html"
+  },
+  {
+    "revision": "42bc94519986f29b09ef",
+    "url": "/parlament-calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5d867a7c836e3071b2d9",
+    "url": "/parlament-calculator/static/js/2.fa6f4e21.chunk.js"
+  },
+  {
+    "revision": "778d532cd9060db112c97dee5a9333db",
+    "url": "/parlament-calculator/static/js/2.fa6f4e21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42bc94519986f29b09ef",
+    "url": "/parlament-calculator/static/js/main.771de511.chunk.js"
+  },
+  {
+    "revision": "932d320841f4ca88b720",
+    "url": "/parlament-calculator/static/js/runtime-main.d4b27be5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/parlament-calculator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2b29198c98fbba0f5982dedea8cd11bc",
+    "url": "/parlament-calculator/static/media/Биримдик.2b29198c.svg"
+  }
+]);
