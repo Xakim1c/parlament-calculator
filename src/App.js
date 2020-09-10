92fe92cc467dbd47b3e6c44c8bbcab609d8dc452
xakim <xakim@pop-os.localdomain>
2020-09-10 09:58:18 +0600

    Add calculations

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import './App.css';
 import electionsConfig from './electionsConfig'
 import Parties from './components/Parties'
 
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useRef } from 'react';
+import { useInterval } from 'react';
+import { generateDataset } from 'react';
+import { d3 } from 'react';
+
 const parliamentSVG = require('parliament-svg')
+
 let parties = 
 {
 	"linke": {
@@ -26,16 +34,47 @@ let parties =
 	}
 }
 
-const svg = parliamentSVG(parties, true)
-//console.log(JSON.stringify(svg))
+const Circles = () => {
+	const [dataset, setDataset] = useState(
+	  generateDataset()
+	)
+	const ref = useRef()
+  
+	useEffect(() => {
+	  const svgElement = d3.select(ref.current)
+	  svgElement.selectAll("circle")
+		.data(dataset)
+		.join("circle")
+		  .attr("cx", d => d[0])
+		  .attr("cy", d => d[1])
+		  .attr("r",  3)
+	}, [dataset])
+  
+	useInterval(() => {
+	  const newDataset = generateDataset()
+	  setDataset(newDataset)
+	}, 2000)
+  
+	return (
+	  <svg
+		viewBox="0 0 100 50"
+		ref={ref}
+	  />
+	)
+  }
 
-//console.log(svg)
+const svg = parliamentSVG(parties, true)
+console.log(JSON.stringify(svg))
+
+console.log(svg)
 
 function App() {
   
   return (
     <div className="App">
+      
       <div>{electionsConfig.distribute_all_votes_message}</div>
+
       <Parties></Parties>
     </div>
   );
--- src/components/Parties.js
+++ src/components/Parties.js
@@ -11,6 +11,8 @@ import TextField from '@material-ui/core/TextField';
 
 import electionsConfig from '../electionsConfig'
 
+import birimdik from './Биримдик.svg';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
@@ -25,33 +27,161 @@ class Parties extends React.Component {
         super(props);
 
         let defaultState = {}
-        electionsConfig.parties.map((value) => {defaultState[value]=0})
-        console.log(defaultState)
+
+        defaultState.percentsLeft=100
+        defaultState.againstAllReached = false
+
+        let parties = {}
+        electionsConfig.parties.map((value) => {
+
+            let partyInfo = {}
+
+            partyInfo.voteResult = 0
+            partyInfo.parlamentResultChairs = 0
+            partyInfo.parlamentResultPercents = 0
+            partyInfo.message = ''
+
+            parties[value]=partyInfo
+        })
+
+        defaultState.parties = parties
 
         this.state = defaultState;
       }    
 
-    render() {
-        return (
-            <List dense className={'Parties'}>
-              {electionsConfig.parties.map((value) => {
-                const labelId = `checkbox-list-secondary-label-${value}`;
-                return (
-                  <ListItem key={value} button>
-                    <ListItemAvatar>
-                      <Avatar
-                        alt={`Avatar n°${value}`}
-                        src={`/static/images/avatar/${value}.jpg`}
-                      />
-                    </ListItemAvatar>
-                    <ListItemText id={labelId} primary={value} />
-                    
-                    <TextField id="outlined-basic" label="Процент голосов" variant="outlined" />
+    voteNumberOnChange = (event) => {
+        console.log(event.target.id)
+
+        const party = event.target.id
+        const parties = {...this.state.parties}  
+        parties[party].voteResult = Number(event.target.value)
+
+        this.setState( {parties: parties} )
+
+        console.log(parties)
+        console.log(this.state)
+
+        //Percents left
+        this.calculateResults(party)
+    }
+
+    calculateResults = (changedParty) => {
+
+        let percentSum = 0
+        let totalPassedParlamentPercent = 0
+
+        Object.keys(this.state.parties).map((party) => {
+            console.log(party)
+
+            let voteResult = this.state.parties[party].voteResult
+            percentSum = percentSum + voteResult
+
+            if (voteResult > electionsConfig.cutoff){
+                totalPassedParlamentPercent = totalPassedParlamentPercent + voteResult     
+            }
+
+         })     
+         
+        let percentsLeft = 100 - percentSum
+        this.setState( {percentsLeft: percentsLeft} )
+
+        const parties = {...this.state.parties} 
+
+        if (percentsLeft == 0) {                     
+
+            //this.setState( {parties: parties} )
+            Object.keys(this.state.parties).map((party) => {
+                console.log(party)
     
-                  </ListItem>
-                );
-              })}
-            </List>
+                let voteResult = this.state.parties[party].voteResult
+                percentSum = percentSum + voteResult
+    
+
+                let parlamentResultPercents = 0  
+                let parlamentResultChairs = 0 
+                let message = electionsConfig.cutoff_message + ' ' + electionsConfig.cutoff + '%'
+
+                if (voteResult > electionsConfig.cutoff){
+                    parlamentResultPercents = voteResult * 100 / totalPassedParlamentPercent  
+                    parlamentResultChairs = electionsConfig.totalChairs * parlamentResultPercents / 100 
+                    message = ''
+                } 
+
+                parties[party].parlamentResultPercents = parlamentResultPercents
+                //parties[party].parlamentResultChairs = Math.round(parlamentResultChairs)
+                parties[party].parlamentResultChairs = Number(parlamentResultChairs).toFixed(1)
+                parties[party].message = message
+            })             
+        } else {
+            
+            this.setState( {parties: parties} )
+            Object.keys(this.state.parties).map((party) => {
+                console.log(party)
+
+                parties[party].parlamentResultPercents = 0
+                parties[party].parlamentResultChairs = 0
+                parties[party].message = ''
+            })             
+        }  
+
+        //Против всех
+        if (parties['Против всех'].parlamentResultPercents < electionsConfig.against_all_cutoff) {
+            parties['Против всех'].message = electionsConfig.against_all_message   
+            this.setState( {againstAllReached: false} ) 
+        } else {
+            parties['Против всех'].message = ''
+            this.setState( {againstAllReached: true} )
+        }
+        
+        this.setState( {parties: parties} )
+    }
+
+    render() {
+
+        const isAgainstAllReached = this.state.againstAllReached;
+
+        return (
+            <div> 
+                <div>Осталось распределить: {this.state.percentsLeft}</div>  
+
+                <b>{isAgainstAllReached ? electionsConfig.against_all_reached_message : ''}</b>
+
+                <List dense className={'Parties'}>
+                {electionsConfig.parties.map((value) => {
+                    const labelId = `label-${value}`;
+                    return (
+                    <ListItem key={value} button>
+                        <ListItemAvatar>
+                        <Avatar
+                            //alt={`Avatar n°${value}`}
+                            src={birimdik}
+                            variant="square"
+                        />
+                        </ListItemAvatar>
+                        <ListItemText id={labelId} primary={value} />
+                        
+                        <TextField  
+                            id={value} 
+                            type ='number'                            
+                            onChange={this.voteNumberOnChange}
+                            label="Процент голосов" 
+                            variant="outlined" /> 
+
+                        <TextField  
+                            id={value} 
+                            value={this.state.parties[value].parlamentResultChairs}
+                            disabled={true}
+                            onChange={this.voteNumberOnChange}
+                            label="Мест в парламенте" 
+                            variant="outlined" /> 
+
+                        <div>{this.state.parties[value].message}</div>
+
+                    </ListItem>
+                    );
+                })}
+                </List>                       
+            </div>
           );
         }
     }    
